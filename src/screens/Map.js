412d251d29c3c31588d16b0eef7f76459a5138c0
CaseyInHaengsin <casey.rowley@kuali.co>
2020-12-23 06:39:10 -0700

    added zoom in and zoom out icons to adjust longitude and latitude delta values.

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import MapView, { Marker } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons';
 import axios from 'axios';
+
 
 const getLocation = async () => {
     let res = await axios.get('http://api.open-notify.org/iss-now.json');
@@ -9,13 +11,13 @@ const getLocation = async () => {
 }
 
 const Map = () => {
-    
     const [location, setLocation] = useState({latitude: 38.8765, longitude: -77.0098});
+    const [zoom, setZoom] = useState(0.00);
 
     useEffect(() => {
         const interval = setInterval(async() => {
             let data = await getLocation();
-            setLocation({latitude: data.latitude, longitude: data.longitude})
+            setLocation({latitude: parseFloat(data.latitude), longitude: parseFloat(data.longitude)})
         }, 10000)
         return () => clearInterval(interval)
     })
@@ -26,23 +28,43 @@ const Map = () => {
             style={styles.map}
             provider={null}
             region={{
-                latitude: parseFloat(`${location.latitude}`),
-                longitude: parseFloat(`${Number(location.longitude)}`),
-                latitudeDelta: 100.00,
-                longitudeDelta: 100.00,
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: zoom,
+                longitudeDelta: zoom,
             }}
             >
                 <Marker 
                     key={location.latitude}
                     coordinate={{
-                        latitude: parseFloat(`${location.latitude}`),
-                        longitude: parseFloat(`${location.longitude}`)
+                        latitude: location.latitude,
+                        longitude: location.longitude
                     }}
                     
                 >
-
                 </Marker>
-                
+                <Feather 
+                    style={styles.zoomIn} 
+                    name="zoom-in" 
+                    color="black"
+                    onPress={() => {
+                        if (zoom > 0){
+                            setZoom(zoom -10)
+                        }
+                    }}
+                />
+                <Feather
+                    style={styles.zoomOut} 
+                    name="zoom-out" 
+                    color="black"
+                    onPress={() => {
+                        if (zoom < 180){
+                            setZoom(zoom + 10)
+                        }
+                        
+                    }}
+
+                />
                
                </MapView>
             
@@ -53,6 +75,20 @@ const Map = () => {
 const styles = StyleSheet.create({
       map: {
         ...StyleSheet.absoluteFillObject,
+      },
+      zoomIn: {
+          alignSelf: 'flex-start',
+          position: 'absolute',
+          bottom: 100,
+          left: 20,
+          fontSize: 50
+      },
+      zoomOut: {
+        alignSelf: 'flex-start',
+        position: 'absolute',
+        left: 20,
+        bottom: 175,
+        fontSize: 50
       }
     })
 
